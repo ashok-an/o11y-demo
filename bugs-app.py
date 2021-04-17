39ce7f05437ecb8f665976809f5d1ffd47dd8d12
Ashok Nagaraj <nashok@cisco.com>
2021-04-17 21:23:53 +0530

    Making MONGODB_URI as env var

--- bugs-app.py
+++ bugs-app.py
@@ -1,10 +1,11 @@
 import json
+import os
 
 from flask import Flask, jsonify, request
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb://localhost:27017/db0"
+app.config["MONGO_URI"] = os.getenv('MONGODB_URI') or "mongodb://localhost:27017/db0"
 mongo = PyMongo(app)
 
 @app.route('/')
